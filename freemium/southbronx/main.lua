043c33520f64404d6e849cde76cac04a30996b49
fiva <105108559+fiva-z@users.noreply.github.com>
2026-05-24 04:40:56 +0200

    Update main.lua

--- freemium/southbronx/main.lua
+++ freemium/southbronx/main.lua
@@ -3607,7 +3607,7 @@ function Library:CreateWindow(...)
             if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
                 task.spawn(Library.Toggle)
             end
-        elseif (Input.KeyCode == Enum.KeyCode.Insert and (not Processed)) then
+        elseif (Input.KeyCode == Enum.KeyCode.Z and (not Processed)) then
             task.spawn(Library.Toggle)
         end
     end))
